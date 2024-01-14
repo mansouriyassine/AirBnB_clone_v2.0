712967b092644364d789bc66380af8495c997590
mansouriyassine <mansouri.yassine1@gmail.com>
2024-01-14 05:17:52 -0800

    101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,59 +1,91 @@
-# Puppet script to set up web servers for the deployment of web_static
+# Configures a web server for deployment of web_static.
 
-# Install Nginx if not already installed
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
 package { 'nginx':
-  ensure => installed,
-}
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
-# Create required directories
 file { '/data':
-  ensure => 'directory',
-}
+  ensure  => 'directory'
+} ->
 
 file { '/data/web_static':
-  ensure => 'directory',
-}
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases':
-  ensure => 'directory',
-}
-
-file { '/data/web_static/shared':
-  ensure => 'directory',
-}
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test':
-  ensure => 'directory',
-}
+  ensure => 'directory'
+} ->
 
-# Create fake HTML file
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
 file { '/data/web_static/releases/test/index.html':
-  content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n',
-}
+  ensure  => 'present',
+  content => "This webpage is found in data/web_static/releases/test/index.htm\n"
+} ->
 
-# Create symbolic link
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test',
-  force  => true,
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# Set ownership
-file { '/data':
-  owner => 'ubuntu',
-  group => 'ubuntu',
-  recurse => true,
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
 
-# Update Nginx configuration
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload  in /var/www/index.html***\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
 file { '/etc/nginx/sites-available/default':
-  content => "server {\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n}\n",
-  notify  => Service['nginx'],
-}
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
 
-# Restart Nginx
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
